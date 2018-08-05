081c0ca7c3f4188b1f80cbe1254254881cf70304
Andrew Waweru <andyweru@gmail.com>
2018-08-05 16:40:40 +0300

    explored python string functions

---
+++ stringfunctions.py
@@ -0,0 +1,43 @@
+# STRING FUNCTIONS
+
+myStr = 'Hello World'
+
+# Capitalise
+print(myStr.capitalize())
+
+# Swap Case
+print(myStr.swapcase())
+
+# Get length
+print(len(myStr))
+
+# replace
+print(myStr.replace('World', "Everyone"))
+
+# Count
+sub = "l"
+print(myStr.count(sub))
+
+#StartWith
+print(myStr.startswith('H'))
+
+#EndstWith
+print(myStr.endswith('d'))
+
+# split to list
+print(myStr.split())
+
+# find
+print(myStr.find('World'))
+
+#index
+print(myStr.index('r'))
+
+#Is all alphanumeric
+print(myStr.isalnum())
+
+#Is all alphabetic
+print(myStr.isalpha())
+
+#Is all numeric
+print(myStr.isalnum())
