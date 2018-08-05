9188cfc521aaf6ad3f3d11d1fdc59808ddc983b1
Andrew Waweru <andyweru@gmail.com>
2018-08-05 12:53:28 +0300

    explored python conditionals

---
+++ conditionals.py
@@ -0,0 +1,47 @@
+# CONDITIONALS    
+
+x = 10
+
+
+# Basic If
+if x < 6:
+    print('This is true')
+
+
+# If Else
+if x < 6:
+    print('This is true')
+else:
+    print('This is false')
+    
+# Elif
+color = 'yellow'
+
+if color == 'red':
+    print('Color is red')
+elif color == 'blue':
+    print('Color is blue')
+else:
+    print('Color is not red or blue')
+
+
+# Nested If
+if x > 5 :
+    if color == 'yellow':
+        print('x is greater than 5 and color is yellow')
+
+
+# Logical operators
+if color == 'yellow' and x > 5:
+    print('x is greater than 5 and color is yellow')
+
+
+
+
+
+
+
+
+
+
+
