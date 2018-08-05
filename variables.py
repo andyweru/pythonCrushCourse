4c622e70a1bf527ed7e2808bf6eb8b1e66b2134b
Andrew Waweru <andyweru@gmail.com>
2018-08-05 12:53:02 +0300

    explored python variables

---
+++ variables.py
@@ -0,0 +1,21 @@
+# VARIABLES AND DATA TYPES
+
+greeting = 'Hello World'
+greeting = 'Hello Everyone'
+
+print(greeting)
+
+# Data type
+
+myStr = 'Hello'
+myInt = 23
+myFloat = 1.3
+
+myList = [1,2,3,'Hello']
+myDict = {'a': 1, 'b': 2, 'c': 3}
+
+print(type(myStr), myStr)
+print(type(myInt), myInt)
+print(type(myFloat), myFloat)
+print(type(myList), myList)
+print(type(myDict), myDict)
