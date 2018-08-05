3b1590abee573c89f8e5bace3821e119a24f8f16
Andrew Waweru <andyweru@gmail.com>
2018-08-05 13:25:11 +0300

    explored python loops

---
+++ loops.py
@@ -0,0 +1,21 @@
+# LOOPS
+
+# FOR LOOP
+people = ['John', 'Sara', 'Tim', 'Bob']
+for person in people:
+    print(person)
+    
+# Iterate by seq index
+for i in range(len(people)):
+    print(people[i])
+    
+for i in range(0, 10):
+    print(i)
+    
+
+# WHILE LOOP
+
+count = 0
+while count < 10:
+    print("Count:", count)
+    count = count + 1
